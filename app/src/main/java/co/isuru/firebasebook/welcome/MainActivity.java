3b5be741631ca4c8e9f07cd1492dc3ebb6436f9b
Isuru <isurum.dev@gmail.com>
2016-08-28 07:08:35 +0530

    Added Firebase FCM.

--- app/src/main/java/co/isuru/firebasebook/welcome/MainActivity.java
+++ app/src/main/java/co/isuru/firebasebook/welcome/MainActivity.java
@@ -1,10 +1,14 @@
 package co.isuru.firebasebook.welcome;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -54,4 +58,23 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_open_notifications:
+                Intent startNotificationsActivityIntent = new Intent(MainActivity.this, MessageActivity.class);
+                startActivity(startNotificationsActivityIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
